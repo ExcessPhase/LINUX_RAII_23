22518c076628c7ad32ac7f9fd479664edd68fcc5
Peter Foelsche <uhcu_63@outlook.com>
2025-10-21 13:49:17 -0500

    chained the calls using and_then()

--- main.cpp
+++ main.cpp
@@ -8,44 +8,56 @@ int main(int, char**argv)
 {	using namespace foelsche::linux_ns;
 
 		/// opening a file
-	auto sFD = foelsche::linux_ns::open(argv[1], O_RDONLY);
-		/// checking for failure
-	if (!sFD.has_value())
-	{	std::cout << sFD.error().what() << std::endl;
-		return 1;
-	}
-	auto sSize = [&](const int fd) -> std::expected<std::size_t, std::system_error>
-	{	struct stat st;
-		if (fstat(fd, &st) < 0)
-			return std::unexpected(std::system_error(errno, std::generic_category(), "fstat failed"));
-		return st.st_size;
-	}(sFD.value().m_i);
-	if (!sSize.has_value())
-	{	std::cout << sSize.error().what() << std::endl;
-		return 1;
-	}
-		/// creating the memory mapping
-	auto sMap = foelsche::linux_ns::mmap(
-		nullptr,
-		sSize.value(),
-		PROT_READ,
-		MAP_SHARED,
-		sFD.value().m_i,
-		0
-	);
-		/// checking for success
-	if (sMap.has_value())
+	auto sFdAndMap = foelsche::linux_ns::open(argv[1], O_RDONLY).and_then(
+		[](foelsche::linux_ns::close fd) -> std::expected<std::pair<foelsche::linux_ns::close, std::size_t>, std::system_error>
+		{	struct stat st;
+			if (fstat(fd.m_i, &st) < 0)
+				return std::unexpected(std::system_error(errno, std::generic_category(), "fstat failed"));
+			return std::make_pair(std::move(fd), st.st_size);
+		}).and_then(
+			[](std::pair<foelsche::linux_ns::close, std::size_t> fdAndSize)
+			{	return foelsche::linux_ns::mmap(
+					nullptr,
+					fdAndSize.second,
+					PROT_READ,
+					MAP_SHARED,
+					fdAndSize.first.m_i,
+					0
+				).transform(
+					[&](foelsche::linux_ns::munmap map)
+					{	return std::make_pair(std::move(fdAndSize.first), std::move(map));
+					}
+				);
+			}
+		);
+				/// checking for success
+	if (sFdAndMap.has_value())
 	{	//std::cout << "successful mmap!" << std::endl;
-			/// moving into some common object for RAII purposes
-		const auto sFdAndMap = std::make_pair(std::move(sFD.value()), std::move(sMap.value()));
 			/// and printing the file contents
 		std::cout << std::string_view(
-			static_cast<const char*>(sFdAndMap.second.m_p),
-			sFdAndMap.second.m_iL
+			static_cast<const char*>(sFdAndMap.value().second.m_p),
+			sFdAndMap.value().second.m_iL
 		) << std::endl;
 	}
 	else
-	{	std::cout << sMap.error().what() << std::endl;
+	{	std::cout << sFdAndMap.error().what() << std::endl;
 		return 1;
 	}
 }
+#if 0
+using namespace foelsche::linux_ns;
+
+auto sFdAndMap = open("file.dat", O_RDONLY)
+    .and_then([](close fd) {
+        struct stat st;
+        if (fstat(fd.m_i, &st) < 0)
+            return std::unexpected(std::system_error(errno, std::generic_category(), "fstat failed"));
+        return std::expected<std::pair<close, std::size_t>, std::system_error>{std::make_pair(std::move(fd), st.st_size)};
+    })
+    .and_then([](std::pair<close, std::size_t> fdAndSize) {
+        return mmap(nullptr, fdAndSize.second, PROT_READ, MAP_SHARED, fdAndSize.first.m_i, 0)
+            .transform([&](munmap map) {
+                return std::make_pair(std::move(fdAndSize.first), std::move(map));
+            });
+    });
+#endif
